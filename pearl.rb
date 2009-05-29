f4e077bed7550b854a695c7d3f97b126477cea0e
Jared Grippe <jared@deadlyicon.com>
2009-05-29 10:34:10 -0700

    changes to self referencing class creation

--- pearl.rb
+++ pearl.rb
@@ -3,6 +3,6 @@ require 'ostruct'
 class Pearl < OpenStruct
   def method_missing(meth, *args)
     return super  if meth.to_s =~ /=$/
-    return self.send("#{meth}=", Pearl.new(args))
+    return self.send("#{meth}=", self.class.new(args))
   end
 end
